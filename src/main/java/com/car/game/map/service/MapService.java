4115d0c9504665a7edd4551fb35c7bda11208bee
bystrzakk <bystry69@gmail.com>
2018-04-24 13:09:39 +0200

    Update MapService.java

--- src/main/java/com/car/game/map/service/MapService.java
+++ src/main/java/com/car/game/map/service/MapService.java
@@ -9,6 +9,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Arrays;
 
 @Log
 @Service
