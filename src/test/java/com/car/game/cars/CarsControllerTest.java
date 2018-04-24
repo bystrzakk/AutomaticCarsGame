5ad4444d00a3754618e6167ef2d615f794850490
p.kedziersk2 <p.kedziersk2@samsung.com>
2018-04-24 15:11:53 +0200

    CarController CarService MapController

--- src/test/java/com/car/game/cars/CarsControllerTest.java
+++ src/test/java/com/car/game/cars/CarsControllerTest.java
@@ -2,9 +2,17 @@ package com.car.game.cars;
 
 import com.car.game.Application;
 import com.car.game.cars.dto.CarDto;
+import com.car.game.cars.dto.CarMoveDto;
+import com.car.game.cars.dto.CarSetup;
 import com.car.game.cars.service.CarsService;
 import com.car.game.common.enums.CarType;
+import com.car.game.common.enums.Direction;
+import com.car.game.common.enums.Move;
+import com.car.game.common.model.CarPk;
 import com.car.game.common.repository.CarRepository;
+import com.car.game.game.ActualInformation;
+import com.car.game.game.MapInformation;
+import com.car.game.game.Position;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +27,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import static com.car.game.common.enums.Direction.N;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,6 +36,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.*;
 
@@ -95,8 +105,60 @@ public class CarsControllerTest {
                 .andExpect(status().isNoContent());
     }
 
+    @Test
+    public void testPutCarInMap() throws Exception {
+        ActualInformation.setActualMapName("testMap");
+        ActualInformation.setConcuretnHashMapGame(getMap());
+
+        mockMvc.perform(post("/car/first/setup")
+                .contentType(contentType)
+                .content(json(getCarSetup())))
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void testMoveCarOnMap() throws Exception {
+//        ActualInformation.setActualMapName("testMap");
+//        ActualInformation.setConcuretnHashMapGame(getMap());
+//
+//        mockMvc.perform(post("/car/move")
+//                .contentType(contentType)
+//                .content(json(getCarMove())))
+//                .andExpect(status().isOk());
+//    }
+
+    private ConcurrentHashMap<Position, MapInformation> getMap(){
+        ConcurrentHashMap<Position, MapInformation> map = new ConcurrentHashMap<>();
+        map.put(new Position(0,0), getMapInformation());
+        return map;
+    }
+
+    private MapInformation getMapInformation(){
+        MapInformation mapInformation = new MapInformation();
+        mapInformation.setIsWall(Boolean.FALSE);
+        mapInformation.setDirection(Direction.N);
+        mapInformation.setIsCrashed(Boolean.FALSE);
+        return mapInformation;
+    }
+
     private CarDto getCarDto(){
         return new CarDto("testCarName", CarType.NORMAL);
+    }
+
+    private CarSetup getCarSetup(){
+        return new CarSetup(getCarDto(),"testMapName", new Position(0,0));
+    }
+
+    private CarMoveDto getCarMove(){
+        return new CarMoveDto(getCarPk(),getMove());
+    }
+
+    private CarPk getCarPk(){
+        return new CarPk("testCarName",CarType.NORMAL);
+    }
+
+    private Move getMove(){
+        return Move.TURN_LEFT;
     }
 
     protected String json(Object o) throws IOException {
